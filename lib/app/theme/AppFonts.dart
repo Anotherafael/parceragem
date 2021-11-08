aefc2232a83649a7f76054fad8a2abc1da863787
Beckmann <henriquebeckmann10@gmail.com>
2021-11-08 16:33:24 -0300

    :heavy_plus_sign: Adicionando google fonts

---
+++ lib/app/theme/AppFonts.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+class AppFonts{
+  static final TextStyle normalPrimary = GoogleFonts.montserrat(fontSize: 16);
+  static final TextStyle dropDownPrimary = GoogleFonts.montserrat(fontSize: 12);
+  static final TextStyle buttonPrimary = GoogleFonts.montserrat(fontSize: 24);
+}
