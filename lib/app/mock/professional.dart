5cda7a3d14ac8883c45c5149877d180568b59b70
Beckmann <henriquebeckmann10@gmail.com>
2021-11-08 16:25:44 -0300

    :rotating_light: Adicionando dados mockados

---
+++ lib/app/mock/professional.dart
@@ -0,0 +1,12 @@
+final professionals = [
+  {
+    "id" : 1,
+    "name" : "Itamar",
+    "phone" : "98798709",
+    "email" : "teste01@gmail.com",
+    "photo_path" : "/profile.png",
+    "document_id" : 1,
+    "password" : "***********",
+    "api_token" : 1
+  }
+];
---
+++ lib/app/mock/section.dart
@@ -0,0 +1,14 @@
+final sections = [
+  {
+    "id" : 1,
+    "name" : "Area01"
+  },
+  {
+    "id" : 2,
+    "name" : "Area02"
+  },
+  {
+    "id" : 3,
+    "name" : "Area03"
+  }
+];
