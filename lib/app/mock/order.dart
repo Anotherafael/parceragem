97a64f74e70ef13e607b07f3797dddfa7910b01c
Anotherafael <rafael.afmendonca1994@gmail.com>
2021-11-09 13:26:21 -0300

    🔨 Fix allocations right.

--- lib/app/mock/order.dart
+++ lib/app/mock/order.dart
@@ -1,11 +1,11 @@
 final orders = [
   {
-    "id" : 1,
-    "price" : 20.0,
-    "status" : 1,
-    "date" : "20/10/2021",
-    "hour" : "22:00",
-    "task_id" : 1,
-    "professional_id" : 1
+    "id": 1,
+    "price": 20.0,
+    "status": 1,
+    "date": "20/10/2021",
+    "hour": "22:00",
+    "task_id": 1,
+    "professional_id": 1
   }
 ];
--- lib/app/presentation/shared/components/layout.dart
+++ lib/app/presentation/shared/components/layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import '../../features/home/home_logged.dart';
 import '../../features/home/home_page.dart';
 
