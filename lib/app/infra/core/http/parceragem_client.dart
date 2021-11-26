6acb4f38a27e82050df7ec17de2e681a22f522c8
Beckmann <henriquebeckmann10@gmail.com>
2021-11-26 16:24:40 -0300

    ignorar o ip

--- lib/app/infra/core/http/parceragem_client.dart
+++ lib/app/infra/core/http/parceragem_client.dart
@@ -1,13 +1,14 @@
 import 'package:dio/adapter.dart';
+import 'package:dio/adapter_browser.dart';
 import 'package:dio/dio.dart';
 
 class ParceragemClient extends DioMixin {
   @override
-  HttpClientAdapter get httpClientAdapter => DefaultHttpClientAdapter();
-
+  HttpClientAdapter get httpClientAdapter => BrowserHttpClientAdapter();
+  
   @override
   BaseOptions get options => BaseOptions(
-        baseUrl: 'http://10.0.2.2:8000/api/v1/',
+        baseUrl: 'http://127.0.0.1:8000/api/v1/',
         headers: {
           'content-Type': 'application/json',
         },
